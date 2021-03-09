1d11fddf1b22fe1b3a2c299ec5092a8dd019d245
Thomas Spicer <3750968+tspicer@users.noreply.github.com>
2021-03-09 12:48:43 -0500

    remove freshclam as daemon for install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -58,7 +58,7 @@ function freshclam() {
     echo "Clamd files are too old. Updating..."
     echo "OK: Running freshclam to update virus databases. This can take a few minutes..."
     sleep 1
-    run="freshclam -d -c 12 -p /run/freshclam.pid" && bash -c "${run}"
+    run="/usr/bin/freshclam -p /run/freshclam.pid" && bash -c "${run}"
   else
     echo "File clamd files are current"
   fi
